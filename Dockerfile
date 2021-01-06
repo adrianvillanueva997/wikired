bcbcea33c5c269bd32d0df5ca8c73f7da5aa5ffc
Adrian Villanueva <adrianvillanueva997@gmail.com>
2021-01-06 23:40:03 +0100

    First bot version

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Multistage docker image building
+# build-env -> dist
+
+FROM node:14.9-alpine as base
+# Building container
+FROM base as builder
+WORKDIR /build
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Production container
+FROM base as dist
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY --from=builder /build/dist ./dist
+CMD ["npm", "run", "prod"]
---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+require('dotenv').config()
+const Twit = require('twit')
+const Cronjob = require('cron').CronJob;
+
+const app = new Twit({
+    consumer_key: process.env.wikired_consumer_key,
+    consumer_secret: process.env.wikired_consumer_secret,
+    access_token: process.env.wikired_access_token,
+    access_token_secret: process.env.wikired_access_secret
+
+})
+
+const tweet = async () => {
+    const text = await axios.get('http://wikired_api/wikired').then(r => {
+        return r.data.text
+    })
+    app.post('statuses/update', {status: text}, function (err: Error, data: any, response: any) {
+    })
+}
+
+
+const job = new Cronjob("0 0-23 * * *", () => {
+    console.log("test")
+    tweet().then();
+})
+job.start();
